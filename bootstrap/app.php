0cbfcc651ed8e730e938d04d59b9b1789a2cb547
mohammadsarwary <rezasarwary12@gmail.com>
2026-01-07 00:08:18 +0430

    Remove HTTP request logging middleware from application bootstrap
    Removed LogHttpRequests middleware from the application middleware stack to disable logging of all API requests.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,9 +21,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'api/*',
         ]);
-
-        // Log all API requests
-        $middleware->append(\Illuminate\Logging\LogHttpRequests::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
